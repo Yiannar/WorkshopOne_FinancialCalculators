fedb89114bd81348f23b1669d5850b022659d8b4
Yianna <Yiannarichey@pursuit.org>
2024-04-12 20:45:01 -0400

    First Calculator

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,28 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Enter your interest: ");
+        float interest = scanner.nextFloat();
+
+        System.out.print("Enter your mortgage term: ");
+        float mortgageTerm = scanner.nextFloat();
+
+        float monthlyInterestRate = interest / 100 / 12;
+        float numberOfMonthlyPayments = mortgageTerm * 12;
+
+        float mortgage = mortgageFormula(principal, monthlyInterestRate, numberOfMonthlyPayments);
+        System.out.printf("Your monthly mortgage payment is: %.2f", mortgage);
+    }
+
+    public static float mortgageFormula(int principal, float monthlyInterestRate, float numberOfMonthlyPayments) {
+        return principal * monthlyInterestRate * (float) Math.pow(1 + monthlyInterestRate, numberOfMonthlyPayments)
+                / ((float) Math.pow(1 + monthlyInterestRate, numberOfMonthlyPayments) - 1);
     }
 }
